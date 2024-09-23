fd672f777ccfafbccc6d2494653af24cdffd9e43
Ilya Bulygin <ibouligin@gmail.com>
2024-09-23 19:56:49 -0400

    installed whitenoise

--- cs412/settings.py
+++ cs412/settings.py
@@ -121,6 +121,13 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
