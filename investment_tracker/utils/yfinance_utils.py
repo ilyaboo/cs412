1ff077c3cc5ddce6a66b24fd4da1ce999a8015c4
Ilya Bulygin <ibouligin@gmail.com>
2024-12-10 06:13:56 -0500

    added loop for price fetching to address the issue with Heroku not getting the price sometimes

--- investment_tracker/utils/yfinance_utils.py
+++ investment_tracker/utils/yfinance_utils.py
@@ -3,22 +3,26 @@ import yfinance as yf
 def get_latest_stock_price(ticker: str) -> float:
     """ returns latest stock price """
 
-    try:
-        stock = yf.Ticker(ticker)
-        return stock.fast_info['lastPrice']
-    except Exception as e:
-        print(f"Error fetching data for {ticker}: {e} (stock)")
-        return None
+    # to address heroku not being able to get a response from API call
+    for _ in range(50):
+        try:
+            stock = yf.Ticker(ticker)
+            return stock.fast_info['lastPrice']
+        except Exception as e:
+            print(f"Error fetching data for {ticker}: {e} (stock)")
+    return None
     
 def get_latest_crypto_price(ticker: str) -> float:
     """ returns latest crypto price """
 
-    try:
-        crypto = yf.Ticker(ticker + "-USD")
-        return crypto.fast_info['lastPrice']
-    except Exception as e:
-        print(f"Error fetching data for {ticker}: {e} (crypto)")
-        return None
+    # to address heroku not being able to get a response from API call
+    for _ in range(50):
+        try:
+            crypto = yf.Ticker(ticker + "-USD")
+            return crypto.fast_info['lastPrice']
+        except Exception as e:
+            print(f"Error fetching data for {ticker}: {e} (crypto)")
+    return None
     
 def get_historical_prices(ticker: str, period: str = "3mo", interval: str = "1d", type: str = "stock"):
     """ fethcing historical prices for the given ticker """
